7a0b88584765978e45c2310144e59fed65d616f0
naveenSDE <naveenkumars2308@gmail.com>
2026-03-16 17:02:56 +0530

    updated db.py

--- db.py
+++ db.py
@@ -1,14 +1,16 @@
 import pymysql
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+
 def get_db():
     conn = pymysql.connect(
-        host=os.getenv("DB_HOST"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        database=os.getenv("DB_NAME")
-
-
+        host=os.getenv("MYSQLHOST"),
+        user="root",
+        password=os.getenv("MYSQLPASSWORD"),
+        database=os.getenv("MYSQLDATABASE"),
+        port=int(os.getenv("MYSQLPORT")),
+        cursorclass=pymysql.cursors.Cursor
     )
     return conn
